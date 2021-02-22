a23c344ee543d28dafce2ed4f6064d5386824d8b
Rafael Diehl <rafaeljuliani1984@gmail.com>
2021-02-22 20:31:25 -0300

    Create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Button } from './components/Button';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Button color="tomato">Botão</Button>
+      <Button color="#000">Button</Button>
+      <Button>Botón</Button>
+    </>
   );
 }
 
