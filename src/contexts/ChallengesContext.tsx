8a317388bd72908e40a48b59c564431ab4ee2b59
Rafael Diehl <rafaeljuliani1984@gmail.com>
2021-02-26 21:42:07 -0300

    Create web notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, ReactNode } from 'react';
+import { useState, useEffect, createContext, ReactNode } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -35,6 +35,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -44,6 +48,14 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+
+    if (Notification.permission == 'granted') {
+      new Notification(`Novo desafio ${challenge.type == 'body' ? '💪' : '👁'}`, {
+        body: `Valendo ${challenge.amount} xp!`,
+        silent: false,
+        icon: 'favicon.png'
+      })
+    }
   }
 
   function resetChallenge() {
