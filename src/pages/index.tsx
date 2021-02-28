fb0e22de4247dc56502ac6e52d39a138822fcd43
Rafael Diehl <rafaeljuliani1984@gmail.com>
2021-02-27 22:57:26 -0300

    Create cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,11 @@ import { Profile } from '../components/Profile';
 import { CountdownProvider } from '../contexts/CountdownContext';
 
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import styles from '../styles/pages/Home.module.css';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -34,4 +35,16 @@ export default function Home() {
       
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+  
+  return {
+    props: {
+      level,
+      currentExperience,
+      challengesCompleted
+    }
+  }
 }
